13bcc24c9c432308fb93c12859695af27ab2e76a
tetondan <dfrehner1@gmail.com>
2015-12-18 16:52:51 -0800

    'added client and added create tables'

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,25 @@
 CREATE DATABASE chat;
 
 USE chat;
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY, 
+  msg_text TEXT, 
+  room_id INTEGER, 
+  user_id INTEGER, 
+  FOREIGN KEY (room_id) REFERENCES rooms (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+
 );
 
 /* Create other tables and define schemas for them here! */
